ee9b4a19f0654a5c4be3e7e98b9f54de57dd9ea0
joshiujjawal22 <joshiujjawal22@gmail.com>
2020-04-28 12:55:07 +0530

    Update acc. to rubric

--- Task0.py
+++ Task0.py
@@ -21,11 +21,9 @@ Print messages:
 
 """
 
-
-if __name__ == '__main__':
-    print("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
-    print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(
-        calls[len(calls)-1][0], calls[len(calls)-1][1],
-        calls[len(calls)-1][2], calls[len(calls)-1][3]))
+print("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
+print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(
+    calls[len(calls)-1][0], calls[len(calls)-1][1],
+    calls[len(calls)-1][2], calls[len(calls)-1][3]))
 
 
